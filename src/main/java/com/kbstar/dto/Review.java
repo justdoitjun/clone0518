14231d408ceb1fd6122cf245c69232557b2df8fd
junstdoitjun <bing74700@gmail.com>
2023-05-18 18:04:28 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/dto/Review.java
@@ -0,0 +1,22 @@
+package com.kbstar.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+@ToString
+@Setter
+@NoArgsConstructor
+public class Review {
+    private int id;
+    private String name;
+    private Date rdate;
+    private String text;
+
+    public Review(String name, String text) {
+        this.name = name;
+        this.text = text;
+    }
+}
